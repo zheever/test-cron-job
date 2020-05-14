6640e4854f11d0172b3b0911b7a0cc81ee077d81
PsAiL <allenps1991@hotmail.com>
2020-05-14 03:04:51 +0000

    Update index.js

--- src/index.js
+++ src/index.js
@@ -22,7 +22,7 @@ User-Agent: Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like
 Referer: https://api.golink.com/wx/once-card
 Accept-Encoding: gzip, deflate
 Accept-Language: zh-CN,zh;q=0.8,en-US;q=0.6,en;q=0.5;q=0.4
-Cookie: advanced-frontend=g2i78a3je5nupbd7q7hkl85i8r; __tins__20201607=%7B%22sid%22%3A%201588779271395%2C%20%22vd%22%3A%2013%2C%20%22expires%22%3A%201588782684566%7D; __51cke__=; __51laig__=13`
+Cookie: advanced-frontend=gmp66vl8uesmohdsgdvi6qkboa; __tins__20201607=%7B%22sid%22%3A%201589425290113%2C%20%22vd%22%3A%201%2C%20%22expires%22%3A%201589427090113%7D; __51cke__=; __51laig__=1`
 );
 
 axios.post(
