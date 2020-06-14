c07707156a45177e732fac553e8e0d76f37a645b
PsAiL <allenps1991@hotmail.com>
2020-06-14 13:36:50 +0800

    Update index.js

--- bin/index.js
+++ bin/index.js
@@ -10,7 +10,7 @@ const hours = new Date().getHours();
   }
 
   if (mins % 9 === 0 /* 每 9 分钟执行一次 */) {
-    run({ shouldNotifyIfSuccess: hours % 1 === 0 /* && mins === 0  每间隔 1 小时 00 分提醒一次 */ })
+    run({ shouldNotifyIfSuccess: hours % 8 === 0 /* && mins === 0  每间隔 8 小时 00 分提醒一次 */ })
     return;
   }
 })();
