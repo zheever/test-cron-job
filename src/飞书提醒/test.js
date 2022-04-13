ecbf2b007b143eb4fb040ecc9edaf2711a955594
PsAiL <allenps1991@hotmail.com>
2022-04-13 19:27:35 +0800

    Update test.js

--- src/飞书提醒/test.js
+++ src/飞书提醒/test.js
@@ -1,1 +1,13 @@
+const axios = require('axios');
 
+module.exports = () => {
+  return axios.post(
+    "https://open.feishu.cn/open-apis/bot/v2/hook/cefcc180-eff8-4b06-905b-a803ce09f4cf",
+    {"msg_type":"text","content":{"text":"request example"}},
+    {
+      headers: {
+       "content-type": "application/json"
+      }
+    }
+  );
+}
