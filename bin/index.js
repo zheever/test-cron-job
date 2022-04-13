af98262b3176eabe4a3d11fd4194a2c2d9c5af4a
sheailun <sheailun@kingsoft.com>
2022-04-13 22:03:23 +0800

    feat: 部署 node_modules 以便减少 faas 下载额外包的时间

--- bin/index.js
+++ bin/index.js
@@ -12,16 +12,4 @@ const hours = new Date().getHours();
    command.handle(type, data);
    return;
   }
-//   if (process.env.NODE_ENV === 'test') {
-//     run({ shouldNotifyIfSuccess: true });
-//     require('../src/预约疫苗预约')();
-//     return;
-//   }
-
-//   if (mins % 9 === 0 /* 每 9 分钟执行一次 */) {
-//     run({ shouldNotifyIfSuccess: hours % 12 === 0  && mins === 10 /* 每间隔 12 小时 10 分提醒一次 */ });
-//     require('../src/预约疫苗预约')();
-//     return;
-//   }
-//   require('../src/飞书提醒/test.js')();
 })();
